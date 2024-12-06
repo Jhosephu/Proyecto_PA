1621d67cc3db323dd729d6e5b06e36a2397553e1
jlpinoc <jlpinoc@unjbg.edu.pe>
2024-12-06 15:34:31 -0500

    Menu 2%

--- utilidades.cpp
+++ utilidades.cpp
@@ -1,4 +1,33 @@
 #include "utilidades.h"
 
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+void mostrarMenuPrincipal()
+{
+	;
+}
+void gestionarProductos()
+{
+	;
+}
+
+void reporteConsultas()
+{
+	;
+}
+
+void operacioneVentas()
+{
+	;
+}
+
+void archivo()
+{
+	;
+}
 
 
