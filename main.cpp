ec6e06aa7274d454d69f6026f17d7de3bdde17d8
jlpinoc <jlpinoc@unjbg.edu.pe>
2024-12-07 21:31:05 -0500

    Menu basico implementado funcional, sin funciones implementadas

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-
+#include "utilidades.h"
 
 int main() {
+	Utilidades util; // Crear una instancia de la clase Utilidades
+	util.mostrarMenuPrincipal();// Llamar al método que muestra el menú
 	
-	return 0;
+	return 0;// Finalizar el programa
 }
--- utilidades.h
+++ utilidades.h
@@ -2,9 +2,21 @@
 #define UTILIDADES_H
 
 #include <vector>
-#include "producto.h"
 
-using namespace std;
+
+class Utilidades
+{
+	public:
+		Utilidades();
+		~Utilidades();
+		void mostrarMenuPrincipal();
+		void gestionarProductos();
+		void reporteConsultas();
+		void operacioneVentas();
+		void archivo();
+		void limpiarPantalla();
+};
+
 
 
 
