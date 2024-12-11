7a8fb6aaa113fde4e412f86bbc5f2dd8cd338fd0
jhosephu <jehuanacunep@unjbg.edu.pe>
2024-12-11 15:42:57 -0500

    actualizando ventas.cpp y agregando la clase transacciones

---
+++ transacciones.h
@@ -0,0 +1,21 @@
+#ifndef TRANSACCIONES_H
+#define TRANSACCIONES_H
+#include "venta.h"
+
+#include <string>
+using namespace std;
+
+class Transacciones {
+    private:
+        vector<Venta> ventas;
+
+    public:
+        Transacciones();
+
+        void registrarVenta(Venta venta);
+        void listarVentas();
+        Venta* buscarVentaPorId(int id);
+        void actualizarStockVentas();
+};
+
+#endif
